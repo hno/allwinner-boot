8b9b226d08ee3bd40ed330f4360d5d5695b6b12f
zhengjiewen <zhengjiewen@Exdroid10.(none)>
2013-02-22 10:44:47 +0800

    1. boot_burn: burn nand or mmc automatically. 2. mmc driver use cpu to copy data

--- boot1/apps/Boot_Burn/update_data/update_data.c
+++ boot1/apps/Boot_Burn/update_data/update_data.c
@@ -19,6 +19,9 @@
 **********************************************************************************************************************
 */
 #include  "update_data.h"
+
+
+static  int  card_sprite_type = 0;
 /*
 ************************************************************************************************************
 *
@@ -35,9 +38,36 @@
 *
 ************************************************************************************************************
 */
+
 __s32 update_flash_init(void)
 {
-	return NAND_Init();
+		int  line_sel;
+		int  card_offset;
+		int  ret;
+	  
+	  if(0==NAND_Init())
+    {
+
+        card_sprite_type=0;
+        return 0;
+    }
+    else
+    {
+      card_sprite_type=1;
+    	ret = wBoot_script_parser_fetch("card2_boot_para", "card_line", &line_sel, 1);
+    	if(ret)
+    	{
+    		line_sel = 4;
+    	}
+    	ret = wBoot_script_parser_fetch("card_boot", "logical_start", &card_offset, 1);
+    	if(ret)
+    	{
+    		card_offset = 40960;
+    	}
+        
+    	return SDMMC_LogicalInit(2, card_offset, line_sel);
+
+    }
 }
 /*
 ************************************************************************************************************
@@ -57,7 +87,18 @@ __s32 update_flash_init(void)
 */
 __s32 update_flash_exit(void)
 {
-	return NAND_Exit();
+	if(!card_sprite_type)
+	{
+		return NAND_Exit();
+	}
+	else if(card_sprite_type == 1)
+	{
+		return SDMMC_LogicalExit(2);
+	}
+	else
+	{
+		return -1;
+	}
 }
 /*
 ************************************************************************************************************
@@ -77,7 +118,20 @@ __s32 update_flash_exit(void)
 */
 __s32 update_flash_read(__u32 nSectNum, __u32 nSectorCnt, void * pBuf)
 {
-	return NAND_LogicRead(nSectNum, nSectorCnt, pBuf);
+	
+		if(!card_sprite_type)
+		{
+			return NAND_LogicRead(nSectNum, nSectorCnt, pBuf);
+		}
+		else if(card_sprite_type == 1)
+		{
+			return SDMMC_LogicalRead(nSectNum, nSectorCnt, pBuf, 2);
+		}
+		else
+		{
+			return -1;
+		}
+
 }
 /*
 ************************************************************************************************************
@@ -97,6 +151,17 @@ __s32 update_flash_read(__u32 nSectNum, __u32 nSectorCnt, void * pBuf)
 */
 __s32 update_flash_write(__u32 nSectNum, __u32 nSectorCnt, void * pBuf)
 {
-	return NAND_LogicWrite(nSectNum, nSectorCnt, pBuf);
+		if(!card_sprite_type)
+		{
+			return NAND_LogicWrite(nSectNum, nSectorCnt, pBuf);
+		}
+		else if(card_sprite_type == 1)
+		{
+			return SDMMC_LogicalWrite(nSectNum, nSectorCnt, pBuf, 2);
+		}
+		else
+		{
+			return -1;
+		}
 }
 
